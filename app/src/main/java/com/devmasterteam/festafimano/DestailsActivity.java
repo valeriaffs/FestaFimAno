11fc94655ac8cf46e1a6a800e70f46abbf9a59bb
vffs <vffs@a.recife.ifpe.edu.br>
2019-01-17 23:12:19 -0300

    Initial commit

---
+++ app/src/main/java/com/devmasterteam/festafimano/DestailsActivity.java
@@ -0,0 +1,13 @@
+package com.devmasterteam.festafimano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DestailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destails);
+    }
+}
