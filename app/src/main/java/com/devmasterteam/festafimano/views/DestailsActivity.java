9a89cc1d3e6d117265480656fbfa505924987b51
vffs <vffs@a.recife.ifpe.edu.br>
2019-01-19 13:47:13 -0300

    Usando Shared Preferences

---
+++ app/src/main/java/com/devmasterteam/festafimano/views/DestailsActivity.java
@@ -0,0 +1,55 @@
+package com.devmasterteam.festafimano.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.devmasterteam.festafimano.R;
+import com.devmasterteam.festafimano.constats.FimDeAnoConstants;
+import com.devmasterteam.festafimano.util.SecurityPreferences;
+
+public class DestailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destails);
+        this.mSecurityPreferences = new SecurityPreferences(this);
+        this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+
+        this.loadDateFromActivity();
+    }
+
+    private void loadDateFromActivity() {
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String presence = extras.getString(FimDeAnoConstants.PRESENCE);
+            if(presence.equals(FimDeAnoConstants.CONFIRMED_WILL_GO)){
+                this.mViewHolder.checkParticipate.setChecked(true);
+            }else{
+                this.mViewHolder.checkParticipate.setChecked(false);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.check_participate){
+            if(mViewHolder.checkParticipate.isChecked()){
+                mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE,FimDeAnoConstants.CONFIRMED_WILL_GO);
+            }else{
+                mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE,FimDeAnoConstants.CONFIRMED_WONT_GO);
+            }
+        }
+    }
+
+    private static class ViewHolder{
+        CheckBox checkParticipate;
+    }
+}
--- app/src/main/java/com/devmasterteam/festafimano/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/festafimano/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.devmasterteam.festafimano;
+package com.devmasterteam.festafimano.views;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.devmasterteam.festafimano.R;
+import com.devmasterteam.festafimano.constats.FimDeAnoConstants;
+import com.devmasterteam.festafimano.util.SecurityPreferences;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViweHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViweHolder.buttonConfirm = findViewById(R.id.button_confirm);
 
         this.mViweHolder.buttonConfirm.setOnClickListener(this);
+        this.mSecurityPreferences = new SecurityPreferences(this);
+        this.verifyPreferences();
+    }
+
+    private void verifyPreferences() {
+        String preference = this.mSecurityPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+        if(preference.equals("")){
+            this.mViweHolder.buttonConfirm.setText(R.string.nao_confirmado);
+        }
+        else if(preference.equals(FimDeAnoConstants.CONFIRMED_WILL_GO)) {
+            this.mViweHolder.buttonConfirm.setText(R.string.sim);
+        }
+        else if(preference.equals(FimDeAnoConstants.CONFIRMED_WONT_GO)) {
+            this.mViweHolder.buttonConfirm.setText(R.string.nao);
+        }
     }
 
     @Override
@@ -28,7 +48,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
 
         if(id == R.id.button_confirm){
+            String presence = mSecurityPreferences.getStoredString(FimDeAnoConstants.PRESENCE);
+
             Intent intent = new Intent(this,DestailsActivity.class);
+            intent.putExtra(FimDeAnoConstants.PRESENCE,presence);
             startActivity(intent);
         }
     }
